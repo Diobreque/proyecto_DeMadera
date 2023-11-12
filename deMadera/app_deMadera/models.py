78eabbbc3b70f79bb9708fec42930e8a674cf75a
Diobreque <117220996+Diobreque@users.noreply.github.com>
2023-11-12 04:48:39 -0300

    first

--- deMadera/app_deMadera/models.py
+++ deMadera/app_deMadera/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    id_categoria = models.AutoField(primary_key=True)
+    nombre_categoria = models.CharField(max_length=50)
+
+
+class Articulos(models.Model):
+    id_articulo = models.AutoField(primary_key=True)
+    nombre_articulo = models.CharField(max_length=50)
+    categoria = models.CharField(max_length=50)
+    
+
